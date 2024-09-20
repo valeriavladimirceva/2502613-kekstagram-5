2fa067f7850a6f2a9a2af29cfece49ef61ddda97
Валерия <V.S.Vladimirtseva@urfu.me>
2024-09-20 22:16:42 +0500

    Создать функции для тренировки

--- js/funstions.js
+++ js/funstions.js
@@ -1,16 +1,16 @@
 function checkLenght (str, maxLenght) {
-  if (str.lenght <= maxLenght) {
+  if (str.length <= maxLenght) {
     return true;
   } else{
     return false;
   }
 }
 checkLenght('ajdkerlkm', 20);
-function checkPolydrome (str){
-  const normalizeStr = str.toLowerCace().replaceAll(' ', '');
+function checkPolydrome (text){
+  const normalizeStr = text.replaceAll(' ', '').toLowerCase();
   let invertedLine = '';
-  for(let i = normalizeStr.lenght - 1; i >= 0; i--){
-    invertedLine += normalizeStr.at(i);
+  for (let i = normalizeStr.length - 1; i >= 0; i--) {
+    invertedLine += normalizeStr[i];
   }
   if (normalizeStr === invertedLine) {
     return true;
