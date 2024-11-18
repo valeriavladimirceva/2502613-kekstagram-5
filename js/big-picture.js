622bb6ec02f4ebb7f1cdb3a1414f74ad111a6f98
Валерия <V.S.Vladimirtseva@urfu.me>
2024-11-18 13:46:00 +0500

    Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,3 +1,5 @@
+const COMMENTS_EACH_DOWNLOAD = 5;
+
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureSocial = bigPicture.querySelector('.big-picture__social');
 const commentCount = bigPicture.querySelector('.social__comment-count');
@@ -5,6 +7,9 @@ const allComments = bigPicture.querySelector('.social__comments');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+
+let currentCommentIndex = 0;
+let currentComments = [];
 
 const createComment = ({avatar, name, message}) => {
   const comment = commentTemplate.cloneNode(true);
@@ -15,20 +20,34 @@ const createComment = ({avatar, name, message}) => {
   return comment;
 };
 
-const showComments = (comments) => {
-  allComments.innerHTML = '';
+const showComments = () => {
+  currentCommentIndex += COMMENTS_EACH_DOWNLOAD;
+  if (currentCommentIndex >= currentComments.length) {
+    commentsLoader.classList.add('hidden');
+    currentCommentIndex = currentComments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
   const fragment = document.createDocumentFragment();
-  comments.forEach(({avatar, name, message}) => {
+  const commentsToShow = currentComments.slice(0, currentCommentIndex);
+
+  commentsToShow.forEach(({avatar, name, message}) => {
     const comment = createComment({avatar, name, message});
     fragment.append(comment);
   });
+
+  allComments.innerHTML = '';
   allComments.append(fragment);
+  commentCount.textContent = `${currentCommentIndex} из ${currentComments.length} комментариев`;
 };
 
 const hideBigPictureModal = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeyDown);
+  currentCommentIndex = 0;
+  currentComments = [];
 };
 
 function onDocumentKeyDown(evt) {
@@ -50,17 +69,22 @@ const showBigPictureModal = (picture) => {
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeyDown);
-  commentsLoader.classList.add('hidden');
-  commentCount.classList.add('hidden');
-
+  commentsLoader.classList.remove('hidden');
+  commentCount.classList.remove('hidden');
   renderBigPicture(picture);
-  showComments(picture.comments);
+  currentComments = picture.comments;
+  showComments();
 };
 
 const onCancelButtonClick = () => {
   hideBigPictureModal();
 };
 
+const onCommentsLoaderClick = () => {
+  showComments();
+};
+
 bigPictureCancel.addEventListener('click', onCancelButtonClick);
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
 
 export { showBigPictureModal };
